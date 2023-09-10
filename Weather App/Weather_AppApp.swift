8bbeefd08664594369baa1459c140149632f5ba3
jturner340 <jturner340@gatech.edu>
2023-09-10 12:18:37 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Jack Turner on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
